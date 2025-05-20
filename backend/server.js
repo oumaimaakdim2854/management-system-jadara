14ebb56e095daf956e1429bfe4361309ac31c8a1
khadija568 <khadija.aboussebre14@gmail.com>
2025-05-20 13:00:29 +0200

    ajouter eventControllers et modifie en server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const connectDB=require("./config/db.js");
+const adminRoutes = require('./routes/adminRoutes');
 
 connectDB()
 
@@ -8,6 +9,13 @@ const express =require("express");
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/events', require('./routes/eventRoutes'));
+app.use('/api/programs', require('./routes/programRoutes'));
+app.use('/api/groups', require('./routes/groupRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
 
 app.listen(port, () => {
     console.log(`Serverur demarre sur le port : ${port}`);
