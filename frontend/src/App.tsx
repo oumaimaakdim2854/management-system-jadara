25c9302a3d97f6d3ea580c39744b3fdae436aefc
khadija568 <khadija.aboussebre14@gmail.com>
2025-05-24 19:57:32 +0200

    Ajout de la page de connexion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import RegisterPage from './app/register/register-page'
+import LoginPage from "./login/login-page"
 
 function App() {
   return (
     <>
       <div>
         <RegisterPage/>
+        <LoginPage/>
       </div>
     </>
   )
