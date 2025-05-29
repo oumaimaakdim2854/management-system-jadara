bab7243a8ed81e4b87a919436305738acbd0e5bf
ouahmanehanane <ouahmaneh2@gmail.com>
2025-05-29 17:20:27 +0100

    add program's grid

--- frontend/src/app/Pages/dashboard.tsx
+++ frontend/src/app/Pages/dashboard.tsx
@@ -16,6 +16,7 @@ import {
 // import TableContent from "@/components/table-content"
 import { Outlet } from "react-router-dom"
 
+
 export default function Page() {
   return (
     <SidebarProvider>
@@ -39,11 +40,7 @@ export default function Page() {
           </Breadcrumb>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
+          
           {/* <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" /> */}
           <Outlet/>
           {/* <TableContent/> */}
